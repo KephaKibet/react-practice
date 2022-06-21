b25ba237574ababa64c8272723f1dcc4eb07b78a
Kepha Kibet <kibet.kepha@live.com>
2022-06-21 22:55:19 +0300

    props with functions

---
+++ react-app-practice/src/myComponent.js
@@ -0,0 +1,21 @@
+// props with functions
+
+
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <button onClick={this.props.onButtonClicked}>Click Me!</button>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
