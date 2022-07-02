b4d27939a09896eed92dc3f3b5260302f3abae4d
Kepha Kibet <kibet.kepha@live.com>
2022-07-02 12:11:12 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -24,29 +24,90 @@
 
 // export default ClockUsingHooks;
 
+///////////////////////useState
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const incrementCount = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   };
+
+//   return (
+//     <div>
+//       <div>{count}</div>
+//       <button onClick={incrementCount}>Increment</button>
+//       <button onClick={decrement}>Decrement</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+/////////////////////  useEffect
+
+
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("black");
 
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
+  useEffect(() => { 
+    const changeColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+    
+    document.addEventListener("click", changeColorOnClick);
 
-  const decrement = () => {
-    setCount(count - 1);
-  };
+    return () => {
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]);
 
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={incrementCount}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
     </div>
   );
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
