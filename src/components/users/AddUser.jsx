7358a8df8ad8a3ea9e9807b33803944375dcc7c7
Kepha Kibet <kibet.kepha@live.com>
2022-11-30 15:51:42 +0300

    AddUser Component

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 const AddUser = props => {
+  const addUserHandler = e => {
+    e.preventDefault()
+  }
 
 
   return(
-  <form>
+  <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age(Years)</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
       
   </form>
  )
